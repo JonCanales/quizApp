7e39af64132a6f4ebe32df579549354d737d347e
Jonathan <andriodjonathan1@gmail.com>
2021-05-04 19:17:01 -0400

    first commit

--- quizApp/ViewController.swift
+++ quizApp/ViewController.swift
@@ -9,10 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trueButton: UIButton!
+    
+    @IBOutlet weak var falseButton: UIButton!
+    
+    @IBOutlet weak var quesions: UILabel!
+    
+    var questionNumber = 0
+    
+    //Structures or Classes allow you to store two different data types together
+    
+    var questionBank = [ [" 4+2 = 7","False"], ["5+3=8","True"], ["Fishes Drink Water","False"] ]
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
+    
+    @objc func updateUI(){
+        quesions.text = questionBank[questionNumber][0]
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+        
+    }
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        /*
+        questionNumber += 1
+        let userInput = sender.currentTitle!
+        let actualAnswer = questionBank[questionNumber][1]
+        updateUI()
+        */
+        let userInput = sender.currentTitle!
+        if userInput == questionBank[questionNumber][1]{
+            sender.backgroundColor = UIColor.green
+        }
+        else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+        if questionNumber + 1 < questionBank.count{
+            questionNumber += 1
+        }
+        else {
+            questionNumber = 0
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
+        
+    }
+   
 
 
 }
